5876e42e5e9f1308712b340c6f35868160f796fb
LeeSD90 <lee.doyle90@gmail.com>
2018-08-23 11:50:44 +0200

    Implement parameter checking for length

--- scripts/ship.js
+++ scripts/ship.js
@@ -1,6 +1,6 @@
-const Ship = () => {
+const Ship = (l = 2) => {
   const ship = {};
-  ship.length = 3;
+  ship.length = (Number.isInteger(l) && l >= 2 && l <= 5) ? l : 2;
   ship.sunk = false;
   ship.hits = [];
 
--- scripts/ship.test.js
+++ scripts/ship.test.js
@@ -2,7 +2,7 @@ const Ship = require('./ship');
 
 test("it creates a default ship object", () => {
   myShip = Ship();
-  expect(myShip.length).toBe(3);
+  expect(myShip.length).toBe(2);
   expect(myShip.sunk).toBe(false);
   expect(myShip.hits).toEqual([]);
 });
@@ -16,13 +16,13 @@ test("it creates a ship with the correct length given a length parameter", () =>
 
 test("it assigns the default ship length when the specified ship length is less than two, above 5 or a non integer value", () => {
  myShip = Ship(-2);
- expect(myShip.length).toBe(3);
+ expect(myShip.length).toBe(2);
  myShip = Ship(2.5);
- expect(myShip.length).toBe(3);
+ expect(myShip.length).toBe(2);
  myShip = Ship(7);
- expect(myShip.length).toBe(3);
+ expect(myShip.length).toBe(2);
  myShip = Ship(3.23);
- expect(myShip.length).toBe(3);
+ expect(myShip.length).toBe(2);
  myShip = Ship("hello");
- expect(myShip.length).toBe(3);
+ expect(myShip.length).toBe(2);
 });
