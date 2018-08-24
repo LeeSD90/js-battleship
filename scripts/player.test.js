d08d24a258a94a21b9bf2f662a3aa1c9d27af15e
LeeSD90 <lee.doyle90@gmail.com>
2018-08-24 17:26:09 +0200

    Refactor test to use setGameboard function

--- scripts/player.test.js
+++ scripts/player.test.js
@@ -3,11 +3,16 @@ const Player = require('./player');
 test('it correctly initializes a new player with a default name', () => {
   p = Player();
   expect(p.name).toBe("Player");
-  expect(p.gameboard).not.toBeUndefined();
 });
 
 test('it correctly initializes a new player with a given name', () => {
   p = Player("Lee");
   expect(p.name).toBe("Lee");
+});
+
+test('it correctly assigns a gameboard to the player', () => {
+  p = Player("Lee");
+  g = { board: [] };
+  p.setGameboard(g);
   expect(p.gameboard).not.toBeUndefined();
 });
