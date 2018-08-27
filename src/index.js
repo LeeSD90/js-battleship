db28d0732aef2d307c23ef53105a52b0f8347165
LeeSD90 <lee.doyle90@gmail.com>
2018-08-27 13:45:12 +0200

    Add random setup of ships on gameboards for each player

--- src/index.js
+++ src/index.js
@@ -4,14 +4,8 @@ const Player =  require('./player');
 const Gameboard = require('./gameboard');
 const Ship = require('./ship');
 
-const newGame = () => {
-  let player1 = Player("p1");
-  let player2 = Player("p2");
-  let gb = Gameboard();
-  let gb2 = Gameboard();
-
-  for(i = 3; i <= 5; i++){
-    console.log("new forloop")
+const placeShips = (board) => {
+  for(let i = 3; i <= 5; i++){
     let running = true;
     while(running){
       try{
@@ -19,20 +13,30 @@ const newGame = () => {
         let x = Math.floor(Math.random() * 10);
         let y = Math.floor(Math.random() * 10);
         let o = Math.floor(((Math.random() * 2) + 1)) == 1 ? "h" : "v";
-        console.log(ship, x, y, o);
-        gb.placeShip(ship, x, y, o);
+        board.placeShip(ship, x, y, o);
         running = false;
-        console.log("done...")
       }
       catch(e){
-        break;
       }
     }
   }
-  console.log(gb);
+  return board;
+}
+
+const newGame = () => {
+  let player1 = Player("p1");
+  let player2 = Player("p2");
+  let gb = Gameboard();
+  let gb2 = Gameboard();
+
+  gb = placeShips(gb);
+  gb2 = placeShips(gb2);
+
   player1.setGameboard(gb);
   player2.setGameboard(gb2);
 
+  console.log(player1.gameboard);
+  console.log(player2.gameboard);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
