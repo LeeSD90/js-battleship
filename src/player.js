36caab82ff10715b614fe13a3136988a035ad518
LeeSD90 <lee.doyle90@gmail.com>
2018-08-28 15:44:58 +0200

    remove unused render function in player

--- src/player.js
+++ src/player.js
@@ -10,11 +10,6 @@ const Player = (n = "Player") => {
     player.opposition = p;
   }
 
-  player.renderBoards = () => {
-    let boards = player.gameboard.render(false) + player.opposition.gameboard.render();
-    return boards;
-  }
-
   return player;
 }
 
